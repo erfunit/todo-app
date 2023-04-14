60dbe34c6ab8fa92f0d22038f54ee016f7cfb864
erfunit <erfunghodoosi@gmail.com>
2023-04-14 13:05:56 +0430

    layout and structure Done!

---
+++ src/components/HOC/Container.jsx
@@ -0,0 +1,9 @@
+import Heading from "../Heading";
+import Input from "../Input";
+import Todos from "../Todos";
+
+const Container = () => {
+  return <div>Container</div>;
+};
+
+export default Container;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,7 @@
+import Todo from "./HOC/Todo";
+
+const Todos = () => {
+  return <div>Todos</div>;
+};
+
+export default Todos;
