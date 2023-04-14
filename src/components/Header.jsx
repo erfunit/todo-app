511067343fc76d6e6c7fe92bad2a624a824d2ae3
erfunit <erfunghodoosi@gmail.com>
2023-04-14 13:45:07 +0430

    Header image Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import lightBg from "../assets/lightBg.jpg";
+import darkBg from "../assets/darkBg.jpg";
 
-const Header = () => {
-  return <div>Header</div>;
-};
-
+const Header = () => (
+  <div className="w-full relative h-[35%] overflow-hidden">
+    <div className="h-full w-full bg-gradient-to-br overflow-hidden flex items-center absolute left-0 top-0 from-[#3710BD] to-[#A42395]">
+      <img
+        src={darkBg}
+        className="opacity-[25%] min-h-[100%] object-cover"
+        alt=""
+      />
+    </div>
+  </div>
+);
 export default Header;
